5d832eac7e9527827f4c3e728890234dc77175fa
Dang Xuan Thang <thangyt00@gmail.com>
2024-10-06 11:20:45 +0700

    chore: Refactor data-access-todo service and add tests

--- libs/data-access-todo/src/lib/data-access-todo.service.ts
+++ libs/data-access-todo/src/lib/data-access-todo.service.ts
@@ -6,10 +6,6 @@ import { Injectable } from "@nestjs/common";
 export class DataAccessTodoService {
   constructor(private readonly TodoPrismaClien: TodoPrismaService) {}
 
-  getData() {
-    return { message: 'Welcome to data-access-todo!' };
-  }
-
   findAll() {
     return this.TodoPrismaClien.todo.findMany();
   }
